2ab006bdefdd0aa6664206017f04ee7f06e5f4a6
Michael MAtteis <michaelcmatteis@gmail.com>
2020-09-13 23:21:04 -0500

    finish what i can

--- src/containers/Import.js
+++ src/containers/Import.js
@@ -1,1 +1,21 @@
 // fill out this container
+import {connect} from "react-redux"
+import Import from "../components/Import"
+import {importMakes} from "../redux/actions"
+import {deleteMake} from "../redux/actions"
+
+
+const mapStateToProps = (state)=>{
+  return{
+    makes : state.makes
+  }
+}
+
+const mapDispatchToProps = (dispatch)=>{
+  return {
+    importMakes: ()=>dispatch(importMakes()),
+    deleteMake: (index)=> dispatch(deleteMake(index))
+  }
+}
+
+export default connect(mapStateToProps,mapDispatchToProps)(Import)
